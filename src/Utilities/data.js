451bfb80d14b4f4f4eea82b70039aecabcfb8d8a
“Hindreen” <hndfaw@gmail.com>
2020-10-01 21:17:24 -0400

    Add pagination to transactions

--- src/Utilities/data.js
+++ src/Utilities/data.js
@@ -1,249 +1,257 @@
 const localData = {
-    transactions: [
-        {
-            id: "trsc-1",
-            customerId: "cstmr-1",
-            customerName: "Bret Cannon",
-            date: "9-25-2020",
-            usd: 75
-        },
-        {
-            id: "trsc-2",
-            customerId: "cstmr-2",
-            customerName: "Aneeka Salas",
-            date: "09-20-2020",
-            usd: 105
-        },
-        {
-            id: "trsc-3",
-            customerId: "cstmr-2",
-            customerName: "Aneeka Salas",
-            date: "09-20-2020",
-            usd: 125
-        },
-        {
-            id: "trsc-4",
-            customerId: "cstmr-2",
-            customerName: "Aneeka Salas",
-            date: "08-18-2020",
-            usd: 25
-        },
-        {
-            id: "trsc-5",
-            customerId: "cstmr-2",
-            customerName: "Aneeka Salas",
-            date: "08-5-2020",
-            usd: 120
-        },
-        {
-            id: "trsc-6",
-            customerId: "cstmr-2",
-            customerName: "Aneeka Salas",
-            date: "07-20-2020",
-            usd: 650
-        },
-        {
-            id: "trsc-7",
-            customerId: "cstmr-3",
-            customerName: "Devante Hogg",
-            date: "07-12-2020",
-            usd: 105
-        },
-        {
-            id: "trsc-14",
-            customerId: "cstmr-3",
-            customerName: "Devante Hogg",
-            date: "08-12-2020",
-            usd: 73
-        },
-        {
-            id: "trsc-8",
-            customerId: "cstmr-3",
-            customerName: "Devante Hogg",
-            date: "09-29-2020",
-            usd: 200
-        },
-        {
-            id: "trsc-9",
-            customerId: "cstmr-3",
-            customerName: "Devante Hogg",
-            date: "09-05-2020",
-            usd: 240
-        },
-        {
-            id: "trsc-11",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "07-03-2020",
-            usd: 50
-        },
-        {
-            id: "trsc-29",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "07-03-2019",
-            usd: 346
-        },
-        {
-            id: "trsc-12",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "07-05-2020",
-            usd: 140
-        },
-        {
-            id: "trsc-13",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "08-11-2020",
-            usd: 80
-        },
-        {
-            id: "trsc-15",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "08-13-2020",
-            usd: 20
-        },
-        {
-            id: "trsc-16",
-            customerId: "cstmr-4",
-            customerName: "Gertrude Leon",
-            date: "08-17-2020",
-            usd: 45
-        },
-        {
-            id: "trsc-17",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "09-19-2020",
-            usd: 65
-        },
-        {
-            id: "trsc-18",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "08-19-2020",
-            usd: 35
-        },
-        {
-            id: "trsc-19",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "07-01-2020",
-            usd: 202
-        },
-        {
-            id: "trsc-20",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "09-01-2020",
-            usd: 100
-        },
-        {
-            id: "trsc-21",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "07-08-2020",
-            usd: 800
-        },
-        {
-            id: "trsc-22",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "09-08-2020",
-            usd: 97
-        },
-        {
-            id: "trsc-23",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "07-04-2020",
-            usd: 150
-        },
-        {
-            id: "trsc-24",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "08-016-2020",
-            usd: 25
-        },
-        {
-            id: "trsc-25",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "08-14-2020",
-            usd: 101
-        },
-        {
-            id: "trsc-26",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "08-18-2020",
-            usd: 99
-        },
-        {
-            id: "trsc-27",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "08-21-2020",
-            usd: 50
-        },
-        {
-            id: "trsc-28",
-            customerId: "cstmr-5",
-            customerName: "Kwabena Lees",
-            date: "08-23-2020",
-            usd: 51
-        },
-        {
-            id: "trsc-30",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "02-16-2019",
-            usd: 80
-        },
-        {
-            id: "trsc-31",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "02-28-2019",
-            usd: 124
-        },
-        {
-            id: "trsc-32",
-            customerId: "cstmr-6",
-            customerName: "Nabilah Buckner",
-            date: "05-15-2019",
-            usd: 2500
-        },
-    ],
-    customers: [
-        {
-            id: "cstmr-6",
-            name: "Nabilah Buckner"
-        },
-        {
-            id: "cstmr-1",
-            name: "Bret Cannon"
-        },
-        {
-            id: "cstmr-2",
-            name: "Aneeka Salas"
-        },
-        {
-            id: "cstmr-3",
-            name: "Devante Hogg"
-        },
-        {
-            id: "cstmr-4",
-            name: "Gertrude Leon"
-        },
-        {
-            id: "cstmr-5",
-            name: "Kwabena Lees"
-        },
-    ]
-}
+  transactions: [
+    {
+      id: "trsc-1",
+      customerId: "cstmr-1",
+      customerName: "Bret Cannon",
+      date: "9-25-2020",
+      usd: 75,
+    },
+    {
+      id: "trsc-2",
+      customerId: "cstmr-2",
+      customerName: "Aneeka Salas",
+      date: "09-20-2020",
+      usd: 105,
+    },
+    {
+      id: "trsc-3",
+      customerId: "cstmr-2",
+      customerName: "Aneeka Salas",
+      date: "09-20-2020",
+      usd: 125,
+    },
+    {
+      id: "trsc-4",
+      customerId: "cstmr-2",
+      customerName: "Aneeka Salas",
+      date: "08-18-2020",
+      usd: 25,
+    },
+    {
+      id: "trsc-5",
+      customerId: "cstmr-2",
+      customerName: "Aneeka Salas",
+      date: "08-5-2020",
+      usd: 120,
+    },
+    {
+      id: "trsc-6",
+      customerId: "cstmr-2",
+      customerName: "Aneeka Salas",
+      date: "07-20-2020",
+      usd: 650,
+    },
+    {
+      id: "trsc-7",
+      customerId: "cstmr-3",
+      customerName: "Devante Hogg",
+      date: "07-12-2020",
+      usd: 105,
+    },
+
+    {
+      id: "trsc-8",
+      customerId: "cstmr-3",
+      customerName: "Devante Hogg",
+      date: "09-29-2020",
+      usd: 200,
+    },
+    {
+      id: "trsc-9",
+      customerId: "cstmr-3",
+      customerName: "Devante Hogg",
+      date: "09-05-2020",
+      usd: 240,
+    },
+    {
+      id: "trsc-11",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "07-03-2020",
+      usd: 50,
+    },
+    {
+      id: "trsc-29",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "07-03-2019",
+      usd: 346,
+    },
+    {
+      id: "trsc-12",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "07-05-2020",
+      usd: 140,
+    },
+    {
+      id: "trsc-13",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "08-11-2020",
+      usd: 80,
+    },
+    {
+      id: "trsc-14",
+      customerId: "cstmr-3",
+      customerName: "Devante Hogg",
+      date: "08-12-2020",
+      usd: 73,
+    },
+    {
+      id: "trsc-10",
+      customerId: "cstmr-3",
+      customerName: "Devante Hogg",
+      date: "08-11-2020",
+      usd: 95,
+    },
+    {
+      id: "trsc-15",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "08-13-2020",
+      usd: 20,
+    },
+    {
+      id: "trsc-16",
+      customerId: "cstmr-4",
+      customerName: "Gertrude Leon",
+      date: "08-17-2020",
+      usd: 45,
+    },
+    {
+      id: "trsc-17",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "09-19-2020",
+      usd: 65,
+    },
+    {
+      id: "trsc-18",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "08-19-2020",
+      usd: 35,
+    },
+    {
+      id: "trsc-19",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "07-01-2020",
+      usd: 202,
+    },
+    {
+      id: "trsc-20",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "09-01-2020",
+      usd: 100,
+    },
+    {
+      id: "trsc-21",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "07-08-2020",
+      usd: 800,
+    },
+    {
+      id: "trsc-22",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "09-08-2020",
+      usd: 97,
+    },
+    {
+      id: "trsc-23",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "07-04-2020",
+      usd: 150,
+    },
+    {
+      id: "trsc-24",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "08-016-2020",
+      usd: 25,
+    },
+    {
+      id: "trsc-25",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "08-14-2020",
+      usd: 101,
+    },
+    {
+      id: "trsc-26",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "08-18-2020",
+      usd: 99,
+    },
+    {
+      id: "trsc-27",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "08-21-2020",
+      usd: 50,
+    },
+    {
+      id: "trsc-28",
+      customerId: "cstmr-5",
+      customerName: "Kwabena Lees",
+      date: "08-23-2020",
+      usd: 51,
+    },
+    {
+      id: "trsc-30",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "02-16-2019",
+      usd: 80,
+    },
+    {
+      id: "trsc-31",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "02-28-2019",
+      usd: 124,
+    },
+    {
+      id: "trsc-32",
+      customerId: "cstmr-6",
+      customerName: "Nabilah Buckner",
+      date: "05-15-2019",
+      usd: 2500,
+    },
+  ],
+  customers: [
+    {
+      id: "cstmr-6",
+      name: "Nabilah Buckner",
+    },
+    {
+      id: "cstmr-1",
+      name: "Bret Cannon",
+    },
+    {
+      id: "cstmr-2",
+      name: "Aneeka Salas",
+    },
+    {
+      id: "cstmr-3",
+      name: "Devante Hogg",
+    },
+    {
+      id: "cstmr-4",
+      name: "Gertrude Leon",
+    },
+    {
+      id: "cstmr-5",
+      name: "Kwabena Lees",
+    },
+  ],
+};
 
 export default localData;
